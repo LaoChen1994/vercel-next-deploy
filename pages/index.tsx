c6063c099fff86d6a37abe883821a893796c2a24
Pidan <chenyixiang_cyx@youzan.com>
2023-04-06 17:03:50 +0800

    fix: add http support

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ const Index = () => {
                 <title>佛曰</title>
                 <meta name="description" content="年轻人都在玩的解压神器，给你的心灵做个SPA"/>
                 <link rel="icon" href="http://rsodxk68s.hd-bkt.clouddn.com/1024.png"/>
+                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
             </Head>
             <Home/>
         </>
--- pages/poster/index.tsx
+++ pages/poster/index.tsx
@@ -1,5 +1,36 @@
+import {useEffect, useRef, useState} from "react";
+import classNames from "classnames";
+
+interface IBorderProps {
+    radius: number;
+    width: number;
+}
+
 const Poster = () => {
-    return <div>123</div>
+    const ref = useRef<HTMLCanvasElement | null>(null)
+    const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>();
+
+    const runBorder = (props: IBorderProps) => {
+
+    }
+
+    useEffect(() => {
+        const el = ref.current;
+        if (!el) return;
+
+        const ctx = el.getContext('2d')
+        setCtx(ctx)
+    }, [])
+
+    return (
+        <div className={classNames(
+            'flex flex-col items-center',
+            'bg-black w-full h-[100vh]'
+        )}>
+            <div className="text-white font-bold text-3xl leading-normal">海报绘制测试</div>
+            <canvas ref={ref} />
+        </div>
+    )
 }
 
 export default Poster
