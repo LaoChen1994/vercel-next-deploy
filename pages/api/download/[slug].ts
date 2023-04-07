fd7bb786af2fc3e700721a5f3d4381c69049f184
Pidan <chenyixiang_cyx@youzan.com>
2023-04-07 15:19:47 +0800

    feat: add android download

---
+++ pages/api/download/[slug].ts
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { promisify } from 'util';
+import * as stream from "stream";
+import Fs from 'fs';
+import Path from 'path';
+
+const pipeline = promisify(stream.pipeline);
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const { slug } = req.query
+    if (slug !== 'andriod') {
+        res.status(400);
+        res.json({ msg: '下载链接不存在' })
+    }
+
+    res.setHeader('Content-Type', 'application/octet-stream');
+    res.setHeader('Content-Disposition', 'attachment; filename=worship.apk');
+    await pipeline(
+        Fs.createReadStream(Path.resolve(__dirname, './assets/v1.apk')),
+        res
+    );
+
+}
---
+++ scripts/move.js
@@ -0,0 +1,27 @@
+const Path = require('path');
+const Fs = require('fs')
+
+const AssestPath = Path.resolve(__dirname, '../public/download')
+const DownloadDir = Path.resolve(__dirname, "../.next/server/pages/api/download")
+const DownloadAsset = Path.resolve(DownloadDir, 'assets')
+
+const downloads = Fs.readdirSync(AssestPath)
+
+if (!downloads.length) {
+    process.exit()
+}
+
+if (!Fs.existsSync(DownloadDir)) {
+    process.exit()
+}
+
+if (!Fs.existsSync(DownloadAsset)) {
+    Fs.mkdirSync(Path.resolve(DownloadDir, 'assets'))
+}
+
+downloads.forEach(file => {
+    const filePath = Path.resolve(AssestPath, file);
+    const destPath = Path.resolve(DownloadAsset, file);
+
+    Fs.copyFileSync(filePath, destPath)
+})
