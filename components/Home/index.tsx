1a8a0db600abdc880afe2f38b9050d49dd671fef
Pidan <chenyixiang_cyx@youzan.com>
2023-04-08 20:24:54 +0800

    fix: update apk version

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -11,7 +11,7 @@ const downloadList = [
     {
         title: '安卓下载',
         type: 'link',
-        content: '/download/v1.apk'
+        content: '/download/v2.apk'
     },
     {
         title: '苹果下载',
